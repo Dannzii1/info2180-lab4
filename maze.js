d29faf36715b2afa3d8b6fec1a2a8456e7e96229
dannzii <34076867+dannzii@users.noreply.github.com>
2018-10-22 22:05:26 -0500

    Exercise 2: All boundaries glow red on hover

--- maze.js
+++ maze.js
@@ -3,7 +3,29 @@ window.onload = main;
 function main(){
 
   let leftwall = document.getElementById("boundary1");
-   leftwall.addEventListener("mouseover", function(){
+  leftwall.addEventListener("mouseover", function(){
      this.setAttribute("class", "boundary youlose");
    });
+
+  let wallboundries = document.querySelectorAll(".boundary");
+  let flagdis = -1;
+
+  for (var p = 0; p < wallboundries.length; p++ ){
+    if (wallboundries[p].className.indexOf("example") != -1){
+      flagdis = p;
+    }else{
+      wallboundries[p].addEventListener("mouseover", function(){
+        document.getElementById("status").textContent = "You Lose!"
+        for(var a = 0; a < wallboundries.length; a++ ){
+          if( a != flagdis){
+            wallboundries[a].className = "boundary youlose";
+   				   }
+          }
+       });
+     }
+  }
+
+
+
+
 }
